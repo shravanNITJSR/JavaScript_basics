7b261aeb3b5883d66f8d458ee2c376a2c82a5ee5
shravanNITJSR <118294295+shravanNITJSR@users.noreply.github.com>
2024-01-11 05:27:38 +0000

    basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+let name = "shravan"
+let age = 24
+let weight = 64.5;
+let isLoggedIn = false;
+let bag=null;
+let status;
+
+console.log(typeof(age));
+console.log(typeof(bag)); //object type....
+console.log(typeof(status)); //undefined type...
+
+
+/*
+1=> number ranges from 2^53
+2=> string can be in single '' or double "" 
+3=> null (it`s datatype in not known) 
+    :-typeog(null)==object
+    :- different from empty string
+4=> undefined :- no value is assigned to it
+    :-typrof(undefined is undefined)
+3=> symbol = unique(used in react(in components....))   
+
+*/
---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const fname="cool";
+let a="dude";
+var b=12;
+let x;
+console.log(fname);
+console.table([a,b,x]);
+/*
+1=> const keyword is used for constant datatype
+2=> let is the variable keyword used for variable declartion
+3=> var has scope problem so nit used in todays world
+4=> the variable without the keyword is undefined....
+*/ 
