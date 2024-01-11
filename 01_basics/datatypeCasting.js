bd9c236472d335a561a855e7e2dbd7b49ee93fd9
shravanNITJSR <118294295+shravanNITJSR@users.noreply.github.com>
2024-01-11 05:57:53 +0000

    update

--- 01_basics/datatypeCasting.js
+++ 01_basics/datatypeCasting.js
@@ -1,14 +1,22 @@
 let name = 'shravan21'
 let number = 33
 let istrue = 'true'
+let emptystring =""
+
+
 
 let stringconvert = String(number)
-//console.log(typeof(stringconvert));
+console.log(typeof(stringconvert));
 
 let numberconvert = Number(name);
 console.log(typeof(numberconvert));
 console.log(numberconvert);
 
+
+
+
 /*
-converting string to number can give NaN(Not a Number) even after converting it 
+1=> converting string to number can give NaN(Not a Number) even after converting it
+2=> whenever empty string is converted into boolean type it will give false type..
+    whereas in true in unempty string...
 */
