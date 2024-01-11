a7c6ea416f22bfc70dfcdee7678f8740cc1803c4
shravanNITJSR <118294295+shravanNITJSR@users.noreply.github.com>
2024-01-11 17:52:48 +0000

    string updated

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -6,7 +6,6 @@ console.log(fname + lname);
 //creating string object using new string function 
 let newString = new String("hitesthc");
 
-
 console.log(newString.toLocaleUpperCase());
 console.log(newString.trim());
 
@@ -16,6 +15,17 @@ console.log(substr);
 
  let slicestr = newString.slice(-8,4);
  console.log(slicestr);
+
+let url = "shravan@google.shravan@1712.com";
+console.log(url.replace("@",'#'));
+
+let sname = "/  beast at work";
+console.log(sname.replace(" ",''));
+
+let gameName = 'shravan-parmar-mp'
+//form an aaray of string on basis of spli function on '-'
+console.log(gameName.split('-'));
+
 /*
 1=> in substring starting index has to be positive or zero
 2=> in slicing negative index will represent from the end/last of the string
