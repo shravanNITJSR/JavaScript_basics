019f8997d598a704207826a106fae0aeeebac1a2
shravanNITJSR <118294295+shravanNITJSR@users.noreply.github.com>
2024-01-11 05:54:41 +0000

    typecasting

---
+++ 01_basics/datatypeCasting.js
@@ -0,0 +1,14 @@
+let name = 'shravan21'
+let number = 33
+let istrue = 'true'
+
+let stringconvert = String(number)
+//console.log(typeof(stringconvert));
+
+let numberconvert = Number(name);
+console.log(typeof(numberconvert));
+console.log(numberconvert);
+
+/*
+converting string to number can give NaN(Not a Number) even after converting it 
+*/
